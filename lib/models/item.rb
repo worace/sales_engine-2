cfb49f101ebb4f5c03620abb715fa204343f5978
worace <horace.d.williams@gmail.com>
2015-03-18 22:41:51 -0600

    dealing witha annoying unit price conversions; 22 remaining

--- lib/models/item.rb
+++ lib/models/item.rb
@@ -1,10 +1,19 @@
+require "bigdecimal"
 require_relative "../model"
 
 class Item < Model
   numeric_attrs "id", "merchant_id"
-  data_attrs "created_at", "updated_at", "name", "description", "unit_price"
+  data_attrs "created_at", "updated_at", "name", "description"
 
   def searchable_attributes
-    ["id", "merchant_id"]
+    ["id", "merchant_id", "string_price", "name"]
+  end
+
+  def string_price
+    unit_price.to_s
+  end
+
+  def unit_price
+    BigDecimal.new(data["unit_price"])/100
   end
 end
--- lib/repositories/item_repository.rb
+++ lib/repositories/item_repository.rb
@@ -13,4 +13,12 @@ class ItemRepository < Repository
   def find_by_id(id)
     indices["id"][id].first
   end
+
+  def find_by_unit_price(price)
+    indices["string_price"].fetch(price.to_s,[]).first
+  end
+
+  def find_all_by_name(name)
+    indices["name"].fetch(name,[])
+  end
 end
